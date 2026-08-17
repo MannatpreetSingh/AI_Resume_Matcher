bce8a974a75ae4ff0444abbe2d84a937f19ac38b
Mannatpreet Singh <mannatpreetsingh14@outlook.com>
2026-08-17 20:00:19 +0530

    Initial Flask project setup

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "AI Resume & Job Macther is running! "
+
+
+if __name__== "__main__":
+     app.run(debug=True)
+     
