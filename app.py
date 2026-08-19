8defe5805019c0ec04549bcbb5e8d3ebc6755745
Mannatpreet Singh <mannatpreetsingh14@outlook.com>
2026-08-19 20:26:21 +0530

    Added logout

--- app.py
+++ app.py
@@ -67,6 +67,12 @@ def dashboard():
         "dashboard.html",
         name=session["user_name"]
     )
+@app.route("/logout")
+def logout():
+    
+    session.clear()
+    
+    return redirect(url_for("login"))
 
 if __name__== "__main__":
      app.run(debug=True)
