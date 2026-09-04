9c755b6d62e1a31cc706f16ac91fcbb582e3498b
Mannatpreet Singh <mannatpreetsingh14@outlook.com>
2026-09-04 19:43:55 +0530

    db update for deployment

--- db.py
+++ db.py
@@ -1,11 +1,13 @@
 import mysql.connector
+import os
 
 def get_db_connection():
-    connection=mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        port=3307,
-        database="resume_matcher"
+    connection = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=int(os.getenv("DB_PORT", 4000)),
+        database=os.getenv("DB_NAME"),
+        ssl_ca=os.getenv("DB_SSL_CA")
     )
     return connection
